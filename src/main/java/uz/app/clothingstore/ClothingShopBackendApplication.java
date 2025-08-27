2df2dfab44aaa204b1c706df82349bf2dfadc68a
jahong1r-t <jahongirtorayev1507@gmail.com>
2025-08-27 17:15:05 +0500

    Initial commit

---
+++ src/main/java/uz/app/clothingstore/ClothingShopBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.app.clothingstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothingShopBackendApplication.class, args);
+    }
+
+}
