ee7c78453616af77b04bf7f40b5480e329e96d6e
jahong1r-t <jahongirtorayev1507@gmail.com>
2025-08-31 21:35:40 +0500

    ProductVariantService class created

---
+++ src/main/java/uz/app/clothingstore/service/impl/ProductVariantServiceImpl.java
@@ -0,0 +1,31 @@
+package uz.app.clothingstore.service.impl;
+
+import uz.app.clothingstore.payload.ApiResponse;
+import uz.app.clothingstore.service.ProductVariantService;
+
+public class ProductVariantServiceImpl implements ProductVariantService {
+    @Override
+    public ApiResponse<?> addProductVariant() {
+        return null;
+    }
+
+    @Override
+    public ApiResponse<?> updateProductVariant() {
+        return null;
+    }
+
+    @Override
+    public ApiResponse<?> deleteProductVariant() {
+        return null;
+    }
+
+    @Override
+    public ApiResponse<?> getProductVariantsByProductId(Long productId) {
+        return null;
+    }
+
+    @Override
+    public ApiResponse<?> getProductVariantById(Long variantId) {
+        return null;
+    }
+}
