301ff64108dfaead0d6d9cf7b0d4dbdd69130eaa
jahong1r-t <jahongirtorayev1507@gmail.com>
2025-08-31 23:40:32 +0500

    Statistic logic

---
+++ src/main/java/uz/app/clothingstore/controller/ProductStatsController.java
@@ -0,0 +1,24 @@
+package uz.app.clothingstore.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1/statistic")
+public interface ProductStatsController {
+    @GetMapping("/products/{productId}")
+    ResponseEntity<?> getProductStatistic(@PathVariable Long productId);
+
+    @GetMapping("/products")
+    ResponseEntity<?> getAllProductStatistics();
+
+    @GetMapping("/variants/{variantId}")
+    ResponseEntity<?> getVariantStatistic(@PathVariable Long variantId);
+
+    @GetMapping("/products/{productId}/variants")
+    ResponseEntity<?> getProductVariantStatistics(@PathVariable Long productId);
+
+    @GetMapping("/variants")
+    ResponseEntity<?> getAllVariantStatistics();
+}
